ae3278dab5547d0ace9fd8160ad75478303ebf9a
Kent Rune Henriksen <kentruneh@gmail.com>
2014-08-10 20:29:05 +0200

    Added multicraft option in Value class

--- src/me/johnnywoof/Value.java
+++ src/me/johnnywoof/Value.java
@@ -9,6 +9,7 @@ public class Value {
 	public static long space = Long.MAX_VALUE;
 	public static boolean warn = true;
 	public static boolean shutdown = false;
+	public static boolean multicraft = false;
 	public static final List<String> silents = new ArrayList<String>();
 	
 	public static long getSpaceLeftInMB(){
